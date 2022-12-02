b2edb1e587f4bb1689cfdcc9643742f801382ffd
Kevin Ni <kevin.kni.ni@gmail.com>
2022-12-02 13:41:17 -0500

    MVP Login System

--- src/LoginSystem/AccountManager.java
+++ src/LoginSystem/AccountManager.java
@@ -9,21 +9,33 @@ public class AccountManager {
 
     //take a snapshot of the current user state to save in accounts
     //public User saveUserState {}
-
-    public void login(String username, String password) {
-        if (this.currLoggedin) {
-            System.out.println("Already logged in");
+    public AccountManager() {
+        this.accounts = new HashMap<>();
+    }
+    //all print statements will be replaced with error codes in the future
+    //Error code 0 means login successful
+    //Error code 1 means Already logged in
+    //Error code 3 means User not in system
+    //Error code 2 means incorrect username or password
+    public int login(String username, String password) {
+        if (this.isCurrLoggedin()) {
+            return 1;
+            //System.out.println("Already logged in");
         }
         if (this.accounts.containsKey(username)) {
             if (this.accounts.get(username).containsKey(password)) {
                 this.activeAccount = this.accounts.get(username).get(password);
-                this.activeAccount.setLoggedin(true);
+                this.activeAccount.login();
                 this.setCurrLoggedin(true);
+                return 0;
+                //System.out.println("Login Successful");
             } else {
-                System.out.println("Incorrect username or password");
+                return 2;
+                //System.out.println("Incorrect username or password");
             }
         } else {
-            System.out.println("User not in system, please create a new account");
+            return 3;
+            //System.out.println("User not in system, please create a new account");
         }
     }
 
@@ -32,16 +44,24 @@ public class AccountManager {
             System.out.println("Username is taken");
         } else {
             HashMap<String, Account> passanduser = new HashMap<String, Account>();
-            passanduser.put(password, new Account());
-            //set new account user
+            passanduser.put(password, new Account(username, password)); //new Acoount(new User)
             this.accounts.put(username, passanduser);
         }
     }
-    public boolean isCurrLoggedin() {
+    private boolean isCurrLoggedin() {
         return currLoggedin;
     }
 
     public void setCurrLoggedin(boolean currLoggedin) {
         this.currLoggedin = currLoggedin;
+    }
+
+    public Account getActive() {
+        return this.activeAccount;
+    }
+
+    public void logout(){
+        this.setCurrLoggedin(false);
+        this.activeAccount = null;
     }
 }

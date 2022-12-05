c18461678d51d5f9311b2388c26e60e8547b5a3e
Kevin Ni <kevin.kni.ni@gmail.com>
2022-12-05 17:26:43 -0500

    Fixed Problems from merging main

--- src/StockAppss.java
+++ src/StockAppss.java
@@ -3,7 +3,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import StockView.UserView;
 
-public class StockApp extends Application {
+public class StockAppss extends Application {
 
     UserView view;
 

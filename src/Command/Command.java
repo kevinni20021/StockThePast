c2edd39bd5f66738b11710ab3bfd8283b8f3a0e1
itsdavidhu <107888075+itsdavidhu@users.noreply.github.com>
2022-12-05 16:23:49 -0500

    Delete src directory

--- src/Command/Command.java
+++
@@ -1,7 +0,0 @@
-package src.Command;
-
-import src.StocksData;
-
-public interface Command {
-    void execute(StocksData stock, double amount, String date);
-}
--- src/State/State.java
+++
@@ -1,8 +0,0 @@
-package src.State;
-
-public interface State {
-
-    void getUser(String username, String password);
-
-    String getState();
-}
